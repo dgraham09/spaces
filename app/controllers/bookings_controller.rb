11fd297129aac2c3b6bb7fc0c0ea9d13d1a40257
david graham <davidcgraham09@gmail.com>
2023-08-21 17:53:27 +0200

    BookingControllerCreated

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,14 @@
+class BookingsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @booking = Booking.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/edit'
+  get 'bookings/new'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
