6c03727d258d673779cbe25d0977b977316c8832
Maksim Fomkin <fomaxxi@gmail.com>
2023-08-22 16:04:48 +0200

    creating a dashboard-1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,9 @@
 class BookingsController < ApplicationController
-  before_action :authenticate_user!
-
   def index
   end
 
   def show
     @space = Space.find(params[:space_id])
-
   end
 
   def edit
